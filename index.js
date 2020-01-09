65e9279571b78f5047b67a265523f07a2a186f90
julieLyM <julielyminh@gmail.com>
2020-01-09 10:50:24 +0100

    docker only use nginx

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", function(req, res) {
+app.get("/*", function(req, res) {
   res.json("Hello Docker");
 });
 
--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,3 +1,5 @@
 FROM nginx:1.17.6-alpine
 
 COPY ./index.html /usr/share/nginx/html/index.html
+
+COPY ./nginx.conf /etc/nginx/
